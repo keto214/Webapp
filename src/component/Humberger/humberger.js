86161528396118d0fba2b5b22df4c9c8f84d9de5
chaun <chaun@CHAUQUYNH>
2021-11-24 16:24:36 +0700

    ff

--- src/component/Humberger/humberger.js
+++ src/component/Humberger/humberger.js
@@ -15,13 +15,12 @@ class Humberger extends React.Component {
                          <div className="humberger__menu__logo">
                               <img alt='avatar' src='https://gcdn.pbrd.co/images/gfPOSrYU2F6h.png' />
                               <div><br/>
-                              <a >
                               <h2 class="word">Make by T.Cry Team</h2> <br/><br/>
                               <h2 class="word">Nguyễn Văn Châu - 4501104024</h2><br/>
                               <h2 class="word">Hồ Sỹ Chiến - 4501104027</h2><br/>
                               <h2 class="word">Nguyễn Minh Hiếu - 4501104083</h2><br/>
                               <h2 class="word">Lương Hoàng Quân - 4501104192</h2><br/>
-                              </a>a
+                            
                </div>
                          </div>
                          <nav className="humberger__menu__nav mobile-menu">
